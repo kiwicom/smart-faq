2b73232be90dd4f4de7fe6813850076b3a145c2d
Toni.U <antoni.peya@kiwi.com>
2018-03-14 17:28:29 +0100

    Finished intro page

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,10 +1,36 @@
 // @flow
 import * as React from 'react';
+import css from 'styled-jsx/css';
 import Intro from '../IntroPage';
 
+const style = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+  html,
+  body {
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+  span.inline-icon svg {
+    display: inline-block;
+    vertical-align: bottom;
+  }
+`;
 const Layout = () => (
   <div className="Layout">
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto"
+      rel="stylesheet"
+    />
     <Intro />
+    <style jsx global>
+      {style}
+    </style>
   </div>
 );
 
