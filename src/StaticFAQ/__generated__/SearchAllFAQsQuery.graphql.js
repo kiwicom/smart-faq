ea23b7d999b673fdd01ece3eb180cb6c84ba1499
Joan <jalbertsr@protonmail.ch>
2018-04-11 12:21:58 +0200

    update query fields

--- src/StaticFAQ/__generated__/SearchAllFAQsQuery.graphql.js
+++ src/StaticFAQ/__generated__/SearchAllFAQsQuery.graphql.js
@@ -1,6 +1,6 @@
 /**
  * @flow
- * @relayHash 4217d4203bed3d10e3775530f8a7c29c
+ * @relayHash 168c3915a28241bf668cecfa4c761427
  */
 
 /* eslint-disable */
@@ -9,6 +9,7 @@
 
 /*::
 import type { ConcreteRequest } from 'relay-runtime';
+type FAQArticle_article$ref = any;
 export type Language = ('ar' | 'bg' | 'ca' | 'cs' | 'da' | 'de' | 'el' | 'en' | 'engb' | 'enus' | 'es' | 'esar' | 'et' | 'fi' | 'fr' | 'he' | 'hr' | 'hu' | 'id' | 'is' | 'it' | 'ja' | 'ko' | 'lt' | 'lv' | 'ms' | 'nl' | 'no' | 'pl' | 'pt' | 'ptbr' | 'ptpt' | 'ro' | 'ru' | 'sk' | 'sl' | 'sr' | 'sv' | 'th' | 'tl' | 'tr' | 'uk' | 'vi' | 'zh' | 'zhcn' | 'zhtw' | '%future added value');
 export type SearchAllFAQsQueryVariables = {|
   search?: ?string,
@@ -18,10 +19,8 @@ export type SearchAllFAQsQueryResponse = {|
   +allFAQs: ?{|
     +edges: ?$ReadOnlyArray<?{|
       +node: ?{|
-        +results: ?{|
-          +title: ?string,
-          +articleId: string,
-        |},
+        +id: string,
+        +$fragmentRefs: FAQArticle_article$ref,
       |},
     |}>,
   |},
@@ -37,13 +36,17 @@ query SearchAllFAQsQuery(
   allFAQs(search: $search, language: $language) {
     edges {
       node {
-        results {
-          title
-          articleId
-        }
+        id
+        ...FAQArticle_article
       }
     }
   }
+}
+
+fragment FAQArticle_article on FAQArticle {
+  id
+  title
+  perex
 }
 */
 
@@ -64,54 +67,113 @@ var v0 = [
 ],
 v1 = [
   {
-    "kind": "LinkedField",
-    "alias": null,
-    "name": "allFAQs",
-    "storageKey": null,
-    "args": [
-      {
-        "kind": "Variable",
-        "name": "language",
-        "variableName": "language",
-        "type": "Language"
-      },
-      {
-        "kind": "Variable",
-        "name": "search",
-        "variableName": "search",
-        "type": "String"
-      }
-    ],
-    "concreteType": "AllFAQsConnection",
-    "plural": false,
+    "kind": "Variable",
+    "name": "language",
+    "variableName": "language",
+    "type": "Language"
+  },
+  {
+    "kind": "Variable",
+    "name": "search",
+    "variableName": "search",
+    "type": "String"
+  }
+],
+v2 = {
+  "kind": "ScalarField",
+  "alias": null,
+  "name": "id",
+  "args": null,
+  "storageKey": null
+};
+return {
+  "kind": "Request",
+  "operationKind": "query",
+  "name": "SearchAllFAQsQuery",
+  "id": null,
+  "text": "query SearchAllFAQsQuery(\n  $search: String\n  $language: Language\n) {\n  allFAQs(search: $search, language: $language) {\n    edges {\n      node {\n        id\n        ...FAQArticle_article\n      }\n    }\n  }\n}\n\nfragment FAQArticle_article on FAQArticle {\n  id\n  title\n  perex\n}\n",
+  "metadata": {},
+  "fragment": {
+    "kind": "Fragment",
+    "name": "SearchAllFAQsQuery",
+    "type": "RootQuery",
+    "metadata": null,
+    "argumentDefinitions": v0,
     "selections": [
       {
         "kind": "LinkedField",
         "alias": null,
-        "name": "edges",
+        "name": "allFAQs",
         "storageKey": null,
-        "args": null,
-        "concreteType": "AllFAQsEdge",
-        "plural": true,
+        "args": v1,
+        "concreteType": "FAQArticleConnection",
+        "plural": false,
         "selections": [
           {
             "kind": "LinkedField",
             "alias": null,
-            "name": "node",
+            "name": "edges",
             "storageKey": null,
             "args": null,
-            "concreteType": "AllFAQs",
-            "plural": false,
+            "concreteType": "FAQArticleEdge",
+            "plural": true,
             "selections": [
               {
                 "kind": "LinkedField",
                 "alias": null,
-                "name": "results",
+                "name": "node",
                 "storageKey": null,
                 "args": null,
-                "concreteType": "Results",
+                "concreteType": "FAQArticle",
                 "plural": false,
                 "selections": [
+                  v2,
+                  {
+                    "kind": "FragmentSpread",
+                    "name": "FAQArticle_article",
+                    "args": null
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "SearchAllFAQsQuery",
+    "argumentDefinitions": v0,
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "allFAQs",
+        "storageKey": null,
+        "args": v1,
+        "concreteType": "FAQArticleConnection",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "name": "edges",
+            "storageKey": null,
+            "args": null,
+            "concreteType": "FAQArticleEdge",
+            "plural": true,
+            "selections": [
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "node",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "FAQArticle",
+                "plural": false,
+                "selections": [
+                  v2,
                   {
                     "kind": "ScalarField",
                     "alias": null,
@@ -122,7 +184,7 @@ v1 = [
                   {
                     "kind": "ScalarField",
                     "alias": null,
-                    "name": "articleId",
+                    "name": "perex",
                     "args": null,
                     "storageKey": null
                   }
@@ -134,29 +196,7 @@ v1 = [
       }
     ]
   }
-];
-return {
-  "kind": "Request",
-  "operationKind": "query",
-  "name": "SearchAllFAQsQuery",
-  "id": null,
-  "text": "query SearchAllFAQsQuery(\n  $search: String\n  $language: Language\n) {\n  allFAQs(search: $search, language: $language) {\n    edges {\n      node {\n        results {\n          title\n          articleId\n        }\n      }\n    }\n  }\n}\n",
-  "metadata": {},
-  "fragment": {
-    "kind": "Fragment",
-    "name": "SearchAllFAQsQuery",
-    "type": "RootQuery",
-    "metadata": null,
-    "argumentDefinitions": v0,
-    "selections": v1
-  },
-  "operation": {
-    "kind": "Operation",
-    "name": "SearchAllFAQsQuery",
-    "argumentDefinitions": v0,
-    "selections": v1
-  }
 };
 })();
-(node/*: any*/).hash = 'af46a24c35cf7c379a267fd741ba1c98';
+(node/*: any*/).hash = '4a1c6640b98f4e0d1a280b85be991d31';
 module.exports = node;
