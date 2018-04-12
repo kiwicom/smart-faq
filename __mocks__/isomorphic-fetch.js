e00aedcbb87704282af74df30ee456e8ec4af09e
Joan <jalbertsr@protonmail.ch>
2018-04-12 15:44:01 +0200

    add social logins

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,11 +1,12 @@
 // @flow
 
-import { loginEndpoint } from '../src/helpers/Requests';
+import { loginEndpoint, socialLoginEndpoint } from '../src/helpers/Requests';
 import {
   goodEmail,
   goodPassword,
   defaultToken,
   defaultUserId,
+  goodPayload,
 } from './constants';
 
 const makeResponse = (status = 200, statusText = 'OK', data = {}) => ({
@@ -31,6 +32,22 @@ export default function fetch(url: string, obj: Object): Promise<Object> {
         makeResponse(200, 'OK', {
           error_code: 'INVALID_ARGUMENT_LOGIN',
           message: 'Invalid email address provided.',
+        }),
+      );
+    }
+  } else if (url === socialLoginEndpoint) {
+    const payload = JSON.parse(obj.body);
+    if (JSON.stringify(payload) === JSON.stringify(goodPayload)) {
+      return Promise.resolve(
+        makeResponse(200, 'OK', {
+          authorization_url: 'https://www.kiwi.com/en/?user_id=AAABBBCCC',
+        }),
+      );
+    } else {
+      return Promise.resolve(
+        makeResponse(200, 'OK', {
+          error_code: 'INVALID_ARGUMENT_IN_PAYLOAD',
+          message: 'Invalid payload argument.',
         }),
       );
     }
