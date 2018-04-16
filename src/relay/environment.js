9189a54e632a5b1c5b044dd68af27ec5fba4e133
Toni U <antoni.peya@kiwi.com>
2018-04-16 10:31:47 +0200

    Default GraphQL URI hardcoded

--- src/relay/environment.js
+++ src/relay/environment.js
@@ -4,10 +4,12 @@ import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 import { getSessionToken } from '../helpers/Auth';
 
 require('isomorphic-fetch');
+// used when smart FAQ installed as dependency
+const uri = 'https://graphql.kiwi.com';
 
 const buildFetchQuery = (token: string = '') => {
   return function fetchQuery(operation, variables) {
-    return fetch(process.env.GRAPHQL_URI, {
+    return fetch(process.env.GRAPHQL_URI || uri, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
