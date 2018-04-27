25df6300006568084c0f6004b2d9b445495b2909
Daniel Kantor <github@daniel-kantor.com>
2018-04-27 15:08:45 +0200

    Fix <Card> size when the header is too big (#125)
    * Fix <Card> size when the header is too big
    
    * Revert change to <Card> size rule
    
    * Use ellipsis to cut of too long headers in FAQArticle
    
    * Fix style issues in FAQArticle.js

--- src/StaticFAQ/FAQArticle.js
+++ src/StaticFAQ/FAQArticle.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { graphql, createFragmentContainer } from 'react-relay';
 import { Typography } from '@kiwicom/orbit-components';
+import css from 'styled-jsx/css';
 
 import Card from './../common/Card';
 import routeDefinitions from '../routeDefinitions';
@@ -13,13 +14,22 @@ type Props = {|
   article: FAQArticle_article,
 |};
 
+const style = css`
+  .ellipsis {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    color: #171b1e; // needed because the ... takes the color from the <div>
+  }
+`;
+
 const FAQArticle = (props: Props) => (
   <Link
     to={`${routeDefinitions.FAQ_ARTICLE}/${props.article.id}`}
     style={{ textDecoration: 'none' }}
   >
     <Card>
-      <div>
+      <div className="ellipsis">
         <Typography type="attention" size="large">
           {props.article.title}
         </Typography>
@@ -30,6 +40,7 @@ const FAQArticle = (props: Props) => (
         </Typography>
       </div>
     </Card>
+    <style jsx>{style}</style>
   </Link>
 );
 
