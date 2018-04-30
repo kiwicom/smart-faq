1710e68122f405831393a3c1f8841e62e6d123b2
Daniel Kantor <git@daniel-kantor.com>
2018-04-30 11:42:06 +0200

    Truncate very long breadcrumbs with ellipses (...)

--- src/StaticFAQ/Breadcrumbs.js
+++ src/StaticFAQ/Breadcrumbs.js
@@ -2,11 +2,20 @@
 
 import * as React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
+import css from 'styled-jsx/css';
 
 import Breadcrumb from './Breadcrumb';
 import BackArrow from './BackArrow';
 import { withLoginToken } from '../context/User';
 import type { Breadcrumbs_breadcrumbs } from './__generated__/Breadcrumbs_breadcrumbs.graphql';
+
+const style = css`
+  .breadcrumbs {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+`;
 
 type Props = {|
   breadcrumbs: Breadcrumbs_breadcrumbs,
@@ -20,21 +29,25 @@ const Breadcrumbs = ({ breadcrumbs, currentCategory, loginToken }: Props) => {
 
   return (
     <React.Fragment>
-      {loginToken && <BackArrow id={id} />}
-      <Breadcrumb breadcrumb={{ title: 'Home' }} />
-      {breadcrumbs.map(breadcrumb => {
-        if (breadcrumb && breadcrumb.title) {
-          return (
-            <Breadcrumb
-              key={breadcrumb.id}
-              breadcrumb={{ id: breadcrumb.id, title: breadcrumb.title }}
-            />
-          );
-        }
+      <div className="breadcrumbs">
+        {loginToken && <BackArrow id={id} />}
+        <Breadcrumb breadcrumb={{ title: 'Home' }} />
+        {breadcrumbs.map(breadcrumb => {
+          if (breadcrumb && breadcrumb.title) {
+            return (
+              <Breadcrumb
+                key={breadcrumb.id}
+                breadcrumb={{ id: breadcrumb.id, title: breadcrumb.title }}
+              />
+            );
+          }
 
-        return null;
-      })}
-      <Breadcrumb breadcrumb={{ title: currentCategory }} isCurrent />
+          return null;
+        })}
+        <Breadcrumb breadcrumb={{ title: currentCategory }} isCurrent />
+      </div>
+
+      <style jsx>{style}</style>
     </React.Fragment>
   );
 };
