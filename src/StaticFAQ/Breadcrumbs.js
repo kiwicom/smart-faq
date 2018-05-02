d803adc9054024c855e59191893dff79ce9d39e1
Daniel Kantor <git@daniel-kantor.com>
2018-05-02 15:22:18 +0200

    Remove superfluous React.Fragment from Breadcrumbs.js

--- src/StaticFAQ/Breadcrumbs.js
+++ src/StaticFAQ/Breadcrumbs.js
@@ -28,27 +28,24 @@ const Breadcrumbs = ({ breadcrumbs, currentCategory, loginToken }: Props) => {
   const id = lastCategory && lastCategory.id;
 
   return (
-    <React.Fragment>
-      <div className="breadcrumbs">
-        {loginToken && <BackArrow id={id} />}
-        <Breadcrumb breadcrumb={{ title: 'Home' }} />
-        {breadcrumbs.map(breadcrumb => {
-          if (breadcrumb && breadcrumb.title) {
-            return (
-              <Breadcrumb
-                key={breadcrumb.id}
-                breadcrumb={{ id: breadcrumb.id, title: breadcrumb.title }}
-              />
-            );
-          }
+    <div className="breadcrumbs">
+      {loginToken && <BackArrow id={id} />}
+      <Breadcrumb breadcrumb={{ title: 'Home' }} />
+      {breadcrumbs.map(breadcrumb => {
+        if (breadcrumb && breadcrumb.title) {
+          return (
+            <Breadcrumb
+              key={breadcrumb.id}
+              breadcrumb={{ id: breadcrumb.id, title: breadcrumb.title }}
+            />
+          );
+        }
 
-          return null;
-        })}
-        <Breadcrumb breadcrumb={{ title: currentCategory }} isCurrent />
-      </div>
-
+        return null;
+      })}
+      <Breadcrumb breadcrumb={{ title: currentCategory }} isCurrent />
       <style jsx>{style}</style>
-    </React.Fragment>
+    </div>
   );
 };
 
