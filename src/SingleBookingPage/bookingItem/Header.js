e51ca0c1da99d53751226871b13042eb900dc0ee
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2018-05-23 19:37:12 +0200

    fix "select booking" link

--- src/SingleBookingPage/bookingItem/Header.js
+++ src/SingleBookingPage/bookingItem/Header.js
@@ -49,7 +49,7 @@ const Header = (props: Props) => {
         <ClickAllBooking.Consumer>
           {onClick => (
             <TextLink
-              url=""
+              url={null}
               onClick={onClick}
               size="small"
               title="Select other booking"
