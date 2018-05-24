16f7a437c63364a4714c96d4dd041954671ecf2a
Dino Trojak <dinotrojak@gmail.com>
2018-05-24 13:16:02 +0200

    changed tests to check actual text

--- src/EmailPage/__tests__/EmailPage.test.js
+++ src/EmailPage/__tests__/EmailPage.test.js
@@ -16,10 +16,14 @@ describe('ForgottenPassword', () => {
 
   it('should render recovery link', () => {
     const wrapper = render(<CheckRecoveryLink {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.text > span').text()).toEqual(
+      'We sent a recovery link to kiwi@email.com.',
+    );
   });
   it('should render magic link', () => {
     const wrapper = render(<CheckMagicLink {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.text > span').text()).toEqual(
+      'To sign in, just click the link in the email we sent to kiwi@email.com.',
+    );
   });
 });
