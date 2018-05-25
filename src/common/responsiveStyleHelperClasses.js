39302f71f7efcef4288569e1a9af78234b82a06e
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2018-05-25 15:11:05 +0200

    Refactor .mobileOnly class

--- src/common/responsiveStyleHelperClasses.js
+++ src/common/responsiveStyleHelperClasses.js
@@ -2,16 +2,14 @@
 import css from 'styled-jsx/css';
 
 const responsiveStyleHelperClasses = css`
-  .mobileOnly {
-    display: none;
+  @media only screen and (min-width: 481px) {
+    .mobileOnly {
+      display: none !important;
+    }
   }
-
   @media only screen and (min-width: 320px) and (max-width: 480px) {
     .desktopOnly {
       display: none !important;
-    }
-    .mobileOnly {
-      display: initial !important;
     }
   }
 `;
