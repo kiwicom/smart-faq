3cb8fe7e0d1bd3e2d36cb86949fe98d3dca9829c
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-05-28 11:04:29 +0200

    Changed descriptions of both tests

--- cypress/integration/User_can_search_in_FAQs.js
+++ cypress/integration/User_can_search_in_FAQs.js
@@ -1,13 +1,13 @@
 // @noflow
 /* global cy */
 
-describe('User not logged', () => {
+describe('User can search in FAQs', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.get('[data-cy=btn-nonexistent-booking]').trigger('click');
   });
 
-  it('User can search in FAQs', () => {
+  it(`The user can write and see a list of FAQ's and reset the input with cross button.`, () => {
     const wordToSearch = 'flight';
     cy.get('[data-cy=input-text]').as('input-staticFAQ');
 
