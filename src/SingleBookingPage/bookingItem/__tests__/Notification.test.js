8b70df0f601d1cfb2db6fb23aa22990cf6950018
Radka Gettová <radoslava.gettova@kiwi.com>
2018-05-29 14:09:53 +0200

    formatCountDown dateutil function should support localisation into multiple languages

--- src/SingleBookingPage/bookingItem/__tests__/Notification.test.js
+++ src/SingleBookingPage/bookingItem/__tests__/Notification.test.js
@@ -3,15 +3,19 @@
 import * as React from 'react';
 import { shallow, render } from 'enzyme';
 
-import Notification from '../Notification';
+import { RawNotification } from '../Notification';
 
 describe('Notification', () => {
   it('should render', () => {
-    expect(shallow(<Notification isUrgent hoursLeft={25} />)).toMatchSnapshot();
+    expect(
+      shallow(<RawNotification isUrgent hoursLeft={25} t={() => 'h'} />),
+    ).toMatchSnapshot();
   });
 
   it('should show warning when message is urgent', () => {
-    const wrapper = render(<Notification isUrgent hoursLeft={25} />);
+    const wrapper = render(
+      <RawNotification isUrgent hoursLeft={25} t={key => key} />,
+    );
 
     expect(wrapper.text()).toContain(
       "Don't hesitate to call us if you have an urgent problem.",
@@ -19,7 +23,9 @@ describe('Notification', () => {
   });
 
   it('should show just info when it is not urgent', () => {
-    const wrapper = render(<Notification isUrgent={false} hoursLeft={62} />);
+    const wrapper = render(
+      <RawNotification isUrgent={false} hoursLeft={62} t={key => key} />,
+    );
 
     expect(wrapper.text()).toContain(
       'There is still time to add some nice extras or even change your booking.',
