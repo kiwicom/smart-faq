3fccc911cfe827d50257db3d7c9d3b4db9f47669
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-05-30 14:36:59 +0200

    Changes in the previous test because of a new prop in Input

--- cypress/integration/User_can_search_in_FAQs.js
+++ cypress/integration/User_can_search_in_FAQs.js
@@ -4,12 +4,12 @@
 describe('User can search in FAQs', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.get('[data-cy=btn-nonexistent-booking]').trigger('click');
+    cy.get('[data-cy=btn-nonexistent-booking]').click();
   });
 
   it(`The user can write and see a list of FAQ's and reset the input with cross button.`, () => {
     const wordToSearch = 'flight';
-    cy.get('[data-cy=input-text]').as('input-staticFAQ');
+    cy.get('[data-cy=input-staticFAQ]').as('input-staticFAQ');
 
     cy
       .get('@input-staticFAQ')
@@ -17,7 +17,7 @@ describe('User can search in FAQs', () => {
       .should('have.value', wordToSearch);
 
     cy.get('[data-cy=scrollable-box]').should('be.visible');
-    cy.get('[data-cy=btn-reset-input]').trigger('click');
+    cy.get('[data-cy=btn-reset-input]').click();
     cy.get('@input-staticFAQ').should('have.value', '');
   });
 });
