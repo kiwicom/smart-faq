994ae46cd187d0230308a0a967982bc6e0c5d398
Joan <jalbertsr@protonmail.ch>
2018-05-30 16:08:31 +0200

    new desing for login screen

--- src/SignInPage/index.js
+++ src/SignInPage/index.js
@@ -4,14 +4,22 @@ import * as React from 'react';
 import idx from 'idx';
 import { Link } from 'react-router-dom';
 import css from 'styled-jsx/css';
-import { Typography, SystemMessage } from '@kiwicom/orbit-components';
-import { AlertCircle } from '@kiwicom/orbit-components/lib/icons';
+import {
+  Typography,
+  Text,
+  Heading,
+  SystemMessage,
+  Button,
+} from '@kiwicom/orbit-components';
+import {
+  AlertCircle,
+  Google,
+  Facebook,
+} from '@kiwicom/orbit-components/lib/icons';
 import type { Location } from 'react-router-dom';
 
 import image from '../../static/woman-with-laptop@2x.jpg';
 import chevronRight from '../../static/chevron-right.png';
-import facebookLogo from '../../static/facebook-icon.png';
-import googleLogo from '../../static/google-logo.png';
 import BackButton from '../common/buttons/BackButton';
 import CloseButton from '../common/buttons/CloseButton';
 import { withSocialLogin } from '../context/User';
@@ -31,66 +39,18 @@ const style = css`
     margin: 0px 127px 0px 150px;
   }
   div.text {
-    margin: 52px 40px 32.2px 40px;
+    margin: 68px 40px 32.8px 40px;
   }
-  p.title {
-    color: #171b1e;
-    font-size: 28px;
-    font-weight: bold;
+  .title {
     margin-bottom: 8px;
-    text-align: center;
   }
-  label {
-    font-size: 14px;
-    line-height: 1.43;
-    color: #46515e;
-    display: inline-block;
-    width: 309px;
-    height: 44px;
-  }
-  form {
-    margin: 0px 40px 0px 40px;
-  }
-  div.input {
-    display: inline-block;
-    width: 100%;
-    height: 44px;
-    margin-top: 4px;
-  }
-  form button {
-    width: 83px;
-    height: 44px;
-    border-radius: 3px;
-    background-color: #00a991;
-    font-size: 14px;
-    font-weight: bold;
-    color: #ffffff;
+  .googleButton {
+    margin-bottom: 16px;
   }
   .buttons {
     margin-right: 134px;
     margin-left: 134px;
-    margin-top: 65px;
-  }
-  button {
-    width: 212px;
-    height: 44px;
-    border-radius: 3px;
-    margin-bottom: 26px;
-    border: none;
-    cursor: pointer;
-    display: inline-block;
-  }
-  button.get-help {
-    width: 83px;
-    background-color: #00a991;
-    border: 0;
-    margin-left: 8px;
-  }
-  button.google {
-    background-color: #d24318;
-  }
-  button.facebook {
-    background-color: #4465ad;
+    margin-bottom: 24px;
   }
   div.kiwi-account {
     margin-top: 8px;
@@ -102,31 +62,7 @@ const style = css`
     margin-left: 5px;
     object-fit: contain;
   }
-  span.label {
-    font-size: 14px;
-    font-weight: bold;
-    line-height: 1.43;
-    color: #ffffff;
-    display: block;
-    text-align: left;
-  }
-  .facebook-icon, .google-icon {
-    width: 16px;
-    height: 16px;
-    float: left;
-    margin-left: 13px;
-    margin-right: 13px;
-  }
-  p.or {
-    width: 15px;
-    height: 23px;
-    font-size: 16px;
-    line-height: 1.4;
-    color: #46515e;
-    margin 0 auto;
-    margin-top: 26px;
-    margin-bottom: 24px;
-  }
+
   @media only screen and (min-width: 320px) and (max-width: 480px) {
     .SignIn {
       width: 100%;
@@ -138,39 +74,16 @@ const style = css`
     div.text {
       margin: 64px 16px 33px 16px;
     }
-    form {
-      margin: 0px 16px;
-    }
-    label {
-      margin-bottom: 33px;
-      width: 100%;
-    }
-    div.input {
-      width: 100%;
-      margin-right: 0px;
-      margin-bottom: 8px;
-    }
-    form button {
-      width: 100%;
-    }
     .buttons {
       margin-right: 16px;
       margin-left: 16px;
     }
-    p.or {
-      margin 0 auto;
-      margin-bottom: 20px;
-    }
+
     button {
       width: 100%;
       margin-bottom: 20px;
     }
-    button.get-help {
-      width: 83px;
-      background-color: #00a991;
-      border: 0;
-      margin-left: 0px;
-    }
+
     @media only screen and (width: 320px) {
       .SignIn {
         padding-top: 0px;
@@ -215,29 +128,39 @@ class SignIn extends React.Component<Props> {
           <img alt="Help" src={image} />
         </div>
         <div className="text">
-          <p className="title">Sign in</p>
+          <div className="title">
+            <Heading weight="bold">Sign in</Heading>
+          </div>
+          <Text type="secondary">
+            We need you to sign in to provide you with more personalised help.
+          </Text>
         </div>
         {sessionExpired && this.renderExpiredSession()}
         <div className="buttons">
-          <button className="google" onClick={() => onSocialLogin('google')}>
-            <img
-              className="google-icon"
-              src={googleLogo}
-              alt="Login with google"
+          <div className="googleButton">
+            <Button
+              title="Continue with Google"
+              onClick={() => {
+                onSocialLogin('google');
+              }}
+              variation="bordered"
+              width={212}
+              type="google"
+              Icon={Google}
+              Facebook
             />
-            <span className="label">Continue with Google </span>
-          </button>
-          <button
+          </div>
+          <Button
+            title="Continue with Facebook"
+            onClick={() => {
+              onSocialLogin('facebook');
+            }}
+            type="facebook"
+            width={212}
+            Icon={Facebook}
+            Facebook
             className="facebook"
-            onClick={() => onSocialLogin('facebook')}
-          >
-            <img
-              className="facebook-icon"
-              src={facebookLogo}
-              alt="Login with facebook"
-            />
-            <span className="label">Continue with Facebook </span>
-          </button>
+          />
         </div>
         <div className="kiwi-account">
           <Link to="/kiwi-login" style={{ textDecoration: 'none' }}>
