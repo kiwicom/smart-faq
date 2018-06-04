a53949b5017b57011ac399ec6da2d56361def143
Daniel Kantor <git@daniel-kantor.com>
2018-06-04 16:14:04 +0200

    fix: typo and idx in OneWayTrip

--- src/MobileBookingHeader/OneWayTrip.js
+++ src/MobileBookingHeader/OneWayTrip.js
@@ -1,21 +1,22 @@
 // @flow
 
-import * as React from 'react';
-import { createFragmentContainer, graphql } from 'react-relay';
+import * as React from "react";
+import { createFragmentContainer, graphql } from "react-relay";
+import idx from "idx";
 
-import OneWayTrip_booking from './__generated__/OneWayTrip_booking.graphql';
-import { TripDescriptionStyle } from './commonStyles';
+import OneWayTrip_booking from "./__generated__/OneWayTrip_booking.graphql";
+import { TripDescriptionStyle } from "./commonStyles";
 
 type Props = {|
   booking: OneWayTrip_booking,
 |};
 
 const OneWayTrip = ({ booking }: Props) => {
-  const origion = booking.trip.departure.airport.city.name;
+  const origin = idx(booking.trip.departure, _ => _.airport.city.name) || "";
   const destination = booking.trip.arrival.airport.city.name;
   return (
     <React.Fragment>
-      <div className="TripDescription">{`${origion} to ${destination}`}</div>
+      <div className="TripDescription">{`${origin} to ${destination}`}</div>
       <style jsx>{TripDescriptionStyle}</style>
     </React.Fragment>
   );
