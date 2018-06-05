16d5f375ebf14b82091323a8dbc945242a0128e8
Dino Trojak <dinotrojak@gmail.com>
2018-06-05 13:36:59 +0200

    updated tests

--- src/common/__tests__/Markdown.test.js
+++ src/common/__tests__/Markdown.test.js
@@ -6,7 +6,11 @@ import Markdown from '../Markdown';
 
 describe('Markdown', () => {
   it('should match snapshot', () => {
-    const result = mount(<Markdown># Hello World</Markdown>);
+    const result = mount(
+      <Markdown>
+        # Hello World [this is my url](https://www.kiwi.com/)
+      </Markdown>,
+    );
     expect(result).toMatchSnapshot();
   });
 });
