7b4e82e0507a8223528c0f6ebe0ca1c36659ace0
Dino Trojak <dinotrojak@gmail.com>
2018-06-05 15:20:32 +0200

    added target=blank to all ArticleDetail links (#344)
    * added target=blank to all ArticleDetail links
    
    * updated tests
    
    * updated snapshot
    
    * added replace for all <a> occurences

--- src/common/Markdown.js
+++ src/common/Markdown.js
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react';
+import MarkdownIt from 'markdown-it';
 
-const md = require('markdown-it')({
+const md = MarkdownIt({
   html: true,
   linkify: true,
   typographer: true,
@@ -11,8 +12,13 @@ type Props = {
   children: ?string,
 };
 
+const renderMarkdown = markdown => {
+  const domString = md.render(markdown);
+  return domString.replace(/<a href=/g, '<a target="_blank" href=');
+};
+
 const Markdown = (props: Props) => (
-  <div dangerouslySetInnerHTML={{ __html: md.render(props.children) }} />
+  <div dangerouslySetInnerHTML={{ __html: renderMarkdown(props.children) }} />
 );
 
 export default Markdown;
