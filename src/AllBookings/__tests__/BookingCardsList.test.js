0da9aab6eef3d19b54b17ddc0580a65348c9ea30
Daniel Kantor <git@daniel-kantor.com>
2018-06-06 19:13:26 +0200

    Fix flow error

--- src/AllBookings/__tests__/BookingCardsList.test.js
+++ src/AllBookings/__tests__/BookingCardsList.test.js
@@ -4,10 +4,12 @@ import { shallow } from 'enzyme';
 
 import { RawBookingCardsList } from '../BookingCardsList';
 
+const mockRefType: any = null;
 describe('RawBookingCardsList', () => {
   it('no bookings in list', () => {
     const booking = {
       edges: [],
+      $refType: mockRefType,
     };
     const result = shallow(
       <RawBookingCardsList title="Example" booking={booking} />,
