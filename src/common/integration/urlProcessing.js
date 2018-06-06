503641cd01381dc0c234c4e385f9ed1e8d829b3e
Toni U <antoni.peya@kiwi.com>
2018-06-06 12:09:38 +0200

    Changed URL Regex to filter new frontend integration '?help' query param

--- src/common/integration/urlProcessing.js
+++ src/common/integration/urlProcessing.js
@@ -11,7 +11,7 @@ type Props = {
 
 const SelectUrlBooking = (props: Props) => {
   const urlMatch = window.location.href.match(
-    /.*[kiwi.com|localhost:\d*]\/.*\/account\/bookings\/(\d*)$/,
+    /.*[kiwi.com|localhost:\d*]\/.*\/account\/bookings\/(\d*)\?.*$/,
   );
   const bookingId = urlMatch && urlMatch[1];
   const { wasSelected, setSelected } = props;
