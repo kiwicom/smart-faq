fd8acc395878c8c982c447b4a9e7f823411b66d3
Dino Trojak <dinotrojak@gmail.com>
2018-06-07 19:11:23 +0200

    added skeleton animation for nearest booking (#371)
    * added skeleton animation for nearest booking
    
    * fixed lint issues

--- src/SingleBookingPage/NearestBooking.js
+++ src/SingleBookingPage/NearestBooking.js
@@ -2,8 +2,9 @@
 
 import * as React from 'react';
 import { graphql } from 'react-relay';
+import idx from 'idx';
+import ContentLoader from 'react-content-loader';
 
-import Loader from '../common/Loader';
 import QueryRenderer from '../relay/QueryRenderer';
 import BookingError from './BookingError';
 import BookingDetail from './BookingDetail';
@@ -27,21 +28,42 @@ const query = graphql`
 
 class NearestBooking extends React.Component<Props> {
   renderBooking = (renderState: RenderState) => {
-    if (renderState && renderState.error) {
-      return <BookingError />;
+    let content = null;
+    const booking = idx(renderState.props, _ => _.nearestBooking);
+
+    if (booking) {
+      content = <BookingDetail booking={booking} />;
+    } else if (!booking) {
+      content = <BookingNotFound />;
     }
 
     if (!renderState.props) {
-      return <Loader />;
+      content = (
+        <ContentLoader
+          speed={1}
+          height={400}
+          primaryColor="#e8edf1"
+          secondaryColor="#d9dee2"
+        >
+          <rect x="30" y="30" rx="4" ry="4" width="150" height="13" />
+          <rect x="280" y="30" rx="4" ry="4" width="100" height="13" />
+          <rect x="30" y="55" rx="4" ry="4" width="200" height="20" />
+          <rect x="30" y="110" rx="4" ry="4" width="350" height="50" />
+          <rect x="30" y="200" rx="4" ry="4" width="350" height="50" />
+          <rect x="140" y="270" rx="4" ry="4" width="120" height="25" />
+        </ContentLoader>
+      );
     }
 
-    const booking = renderState.props.nearestBooking;
-
-    if (!booking) {
-      return <BookingNotFound />;
+    if (renderState && renderState.error) {
+      content = <BookingError />;
     }
 
-    return <BookingDetail booking={booking} />;
+    return (
+      <div style={{ height: '100%', backgroundColor: '#f5f7f9' }}>
+        {content}
+      </div>
+    );
   };
 
   render() {
