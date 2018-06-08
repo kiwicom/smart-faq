8e46b26c6cea9f609b3c211a0533e392cbc06b74
Dino Trojak <dinotrojak@gmail.com>
2018-06-08 19:04:08 +0200

    moved BookingLoader to a component

--- src/SingleBookingPage/NearestBooking.js
+++ src/SingleBookingPage/NearestBooking.js
@@ -3,12 +3,12 @@
 import * as React from 'react';
 import { graphql } from 'react-relay';
 import idx from 'idx';
-import ContentLoader from 'react-content-loader';
 
 import QueryRenderer from '../relay/QueryRenderer';
 import BookingError from './BookingError';
 import BookingDetail from './BookingDetail';
 import BookingNotFound from './BookingNotFound';
+import BookingLoader from './BookingLoader';
 import type { NearestBookingQueryResponse as QueryResponseType } from './__generated__/NearestBookingQuery.graphql';
 
 type Props = {||};
@@ -38,21 +38,7 @@ class NearestBooking extends React.Component<Props> {
     }
 
     if (!renderState.props) {
-      content = (
-        <ContentLoader
-          speed={1}
-          height={400}
-          primaryColor="#e8edf1"
-          secondaryColor="#d9dee2"
-        >
-          <rect x="30" y="30" rx="4" ry="4" width="150" height="13" />
-          <rect x="280" y="30" rx="4" ry="4" width="100" height="13" />
-          <rect x="30" y="55" rx="4" ry="4" width="200" height="20" />
-          <rect x="30" y="110" rx="4" ry="4" width="350" height="50" />
-          <rect x="30" y="200" rx="4" ry="4" width="350" height="50" />
-          <rect x="140" y="270" rx="4" ry="4" width="120" height="25" />
-        </ContentLoader>
-      );
+      content = <BookingLoader />;
     }
 
     if (renderState && renderState.error) {
