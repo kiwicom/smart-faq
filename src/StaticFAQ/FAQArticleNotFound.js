cf44907d4212c3ee5fc5cdb0b8bc54e8ee1904e0
Dino Trojak <dinotrojak@gmail.com>
2018-06-12 12:18:49 +0200

    added redirect back to root faq

--- src/StaticFAQ/FAQArticleNotFound.js
+++ src/StaticFAQ/FAQArticleNotFound.js
@@ -1,6 +1,7 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { Heading, Text, Button } from '@kiwicom/orbit-components';
+import { withRouter } from 'react-router-dom';
 import css from 'styled-jsx/css';
 
 const style = css`
@@ -18,25 +19,33 @@ const style = css`
   }
 `;
 
-function openHelpCenter() {
-  var win = window.open('https://www.kiwi.com/helpcenter/', '_blank');
-  win.focus();
+type Props = {
+  history: {
+    push: string => void,
+  },
+};
+
+class ArticleNotFound extends Component<Props> {
+  goToNoBooking = () => {
+    this.props.history.push('/faq/');
+  };
+  render() {
+    return (
+      <div className="notFoundContainer">
+        <div>
+          <Heading>Sorry, that article has ceased to exist</Heading>
+          <div className="textMargin">
+            <Text>
+              It may have been deleted or moved. Or it could just be a temporary
+              technical issue.
+            </Text>
+          </div>
+          <Button onClick={this.goToNoBooking} title="Open the Help Center" />
+        </div>
+        <style jsx>{style}</style>
+      </div>
+    );
+  }
 }
 
-const ArticleNotFound = () => (
-  <div className="notFoundContainer">
-    <div>
-      <Heading>Sorry, that article has ceased to exist</Heading>
-      <div className="textMargin">
-        <Text>
-          It may have been deleted or moved. Or it could just be a temporary
-          technical issue.
-        </Text>
-      </div>
-      <Button onClick={openHelpCenter} title="Open the Help Center" />
-    </div>
-    <style jsx>{style}</style>
-  </div>
-);
-
-export default ArticleNotFound;
+export default withRouter(ArticleNotFound);
