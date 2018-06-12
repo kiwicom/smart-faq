ffd01ef0a38be2fc2609616dc139a79dad804ffc
Daniel Kantor <git@daniel-kantor.com>
2018-06-12 12:55:24 +0200

    Simplify test for replaceWithCurrentDomain

--- src/helpers/__tests__/replaceWithCurrentDomain.test.js
+++ src/helpers/__tests__/replaceWithCurrentDomain.test.js
@@ -2,16 +2,12 @@
 
 import { replaceDomain } from '../replaceWithCurrentDomain';
 
-const testCases = [
-  ['https://example.com', 'https://domain.net', 'https://domain.net/'],
-  ['https://example.com', 'https://domain2.org', 'https://domain2.org/'],
-  ['https://kiwi.com/asdg/4/', 'https://kiwi.org', 'https://kiwi.org/asdg/4/'],
-];
+const URL = 'https://kiwi.com/asdg/4/';
+const domain = 'https://kiwi.org';
+const result = 'https://kiwi.org/asdg/4/';
 
 describe('replaceDomain', () => {
-  testCases.forEach(([URL, domain, result]) => {
-    it(`replaceDomain(${URL}, ${domain})`, () => {
-      expect(replaceDomain(URL, domain)).toEqual(result);
-    });
+  it(`replaceDomain(${URL}, ${domain})`, () => {
+    expect(replaceDomain(URL, domain)).toEqual(result);
   });
 });
