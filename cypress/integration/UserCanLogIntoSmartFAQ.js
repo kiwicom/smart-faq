5589a8a6f955234c5a53fe135259dfb29246cd87
Dino Trojak <dinotrojak@gmail.com>
2018-06-13 13:17:42 +0200

    added cypress tests for bookings

--- cypress/integration/UserCanLogIntoSmartFAQ.js
+++ cypress/integration/UserCanLogIntoSmartFAQ.js
@@ -5,21 +5,61 @@
 */
 
 describe('User can log into SmartFAQ', () => {
-  beforeEach(() => {
-    cy
-      .fixture('UserCanLogIntoSmartFAQ_Mock.json')
-      .then(UserCanLogIntoSmartFAQ_Mock => {
-        cy.mockRequest(UserCanLogIntoSmartFAQ_Mock);
-        cy.get('[data-cy=btn-existent-booking]').click();
-        cy.get('[data-cy=link-kiwi-login]').click();
-      });
-  });
-
   it(`The user should be able to log into SmartFAQ with the correct credentials.`, () => {
+    cy.visit('/');
+    cy.get('[data-cy=btn-existent-booking]').click();
+    cy.get('[data-cy=link-kiwi-login]').click();
+
     cy.get('[data-cy=input-email]').type(Cypress.env('TEST_USER_EMAIL'));
     cy.get('[data-cy=input-password]').type(Cypress.env('TEST_USER_PASSWORD'));
     cy.get('[data-cy=btn-sign-in]').click();
-    cy.wait(1000);
+
     cy.get('[data-cy=nearestBooking]').should('exist');
+  });
+
+  it('checks if upcoming bookings exist', () => {
+    cy
+      .get('[data-cy=btn-other-bookings]')
+      .find('a')
+      .click();
+
+    cy.wait(5000);
+    cy.get('[data-cy=booking-card]').should('exist');
+  });
+
+  it('checks if upcoming booking is displayed', () => {
+    cy
+      .get('[data-cy=upcoming-bookings]')
+      .find('[data-cy=booking-card]')
+      .first()
+      .click();
+
+    cy
+      .get('[data-cy=booking-type]')
+      .find('p')
+      .contains('Upcoming trip');
+
+    cy.get('[data-cy=booking-title]').should('exist');
+    cy.get('[data-cy=btn-manage-booking]').should('exist');
+    cy
+      .get('[data-cy=btn-other-bookings]')
+      .find('a')
+      .click();
+  });
+
+  it('checks if past booking is displayed', () => {
+    cy
+      .get('[data-cy=past-bookings]')
+      .find('[data-cy=booking-card]')
+      .first()
+      .click();
+
+    cy
+      .get('[data-cy=booking-type]')
+      .find('p')
+      .contains('Past trip');
+
+    cy.get('[data-cy=booking-title]').should('exist');
+    cy.get('[data-cy=btn-manage-booking]').should('exist');
   });
 });
