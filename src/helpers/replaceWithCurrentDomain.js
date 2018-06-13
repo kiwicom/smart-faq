d8980871ae1806c92dbdb60524d718be74b3c21e
Daniel Kantor <git@daniel-kantor.com>
2018-06-13 12:31:48 +0200

    Fix style issues

--- src/helpers/replaceWithCurrentDomain.js
+++ src/helpers/replaceWithCurrentDomain.js
@@ -1,7 +1,7 @@
 // @flow
 
 export const replaceDomain = (link: string, domain: string) =>
-  link.replace(/(https?:\/\/)[^\/]+(.*)/, `${domain}\$2`)
+  link.replace(/(https?:\/\/)[^/]+(.*)/, `${domain}$2`);
 
 const replaceWithCurrentDomain = (url: string) =>
   replaceDomain(url, window.location.origin);
