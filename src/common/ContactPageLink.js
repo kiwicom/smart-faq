d3db70a2bcade223a7ccc9a6ebdb649846700af6
Dino Trojak <dinotrojak@gmail.com>
2018-06-14 09:42:11 +0200

    added relative links

--- src/common/ContactPageLink.js
+++ src/common/ContactPageLink.js
@@ -3,44 +3,46 @@
 import * as React from 'react';
 import { NewWindow } from '@kiwicom/orbit-components/lib/icons';
 
+import { withLanguage } from '../context/Language';
+
 type Props = {
   text?: string,
   textColor?: string,
+  language?: string,
 };
 
 const ContactPageLink = ({
   text = 'Having trouble? Contact us',
   textColor = '#171b1e',
-}: Props) => (
-  <div className="contactUs">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://www.kiwi.com/en/content/feedback"
-    >
-      {text}
-      <span className="open-icon">
-        <NewWindow size="small" color="attention" customColor={textColor} />
-      </span>
-    </a>
-    <style jsx>
-      {`
-        .contactUs {
-          text-align: center;
-          margin-top: 25px;
-        }
-        a {
-          color: ${textColor};
-          font-size: 14px;
-          text-decoration: none;
-          font-weight: bold;
-        }
-        a .open-icon {
-          margin-left: 6px;
-        }
-      `}
-    </style>
-  </div>
-);
+  language = 'en',
+}: Props) => {
+  return (
+    <div className="contactUs">
+      <a target="_blank" href={`${language}/content/feedback`}>
+        {text}
+        <span className="open-icon">
+          <NewWindow size="small" color="attention" customColor={textColor} />
+        </span>
+      </a>
+      <style jsx>
+        {`
+          .contactUs {
+            text-align: center;
+            margin-top: 25px;
+          }
+          a {
+            color: ${textColor};
+            font-size: 14px;
+            text-decoration: none;
+            font-weight: bold;
+          }
+          a .open-icon {
+            margin-left: 6px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
-export default ContactPageLink;
+export default withLanguage(ContactPageLink);
