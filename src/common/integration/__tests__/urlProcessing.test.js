58b2d36c8f7add7bfac9f09589d9ea67e5523dd9
Dino Trojak <dinotrojak@gmail.com>
2018-06-15 15:12:33 +0200

    added test for a not valid url

--- src/common/integration/__tests__/urlProcessing.test.js
+++ src/common/integration/__tests__/urlProcessing.test.js
@@ -23,4 +23,20 @@ describe('UrlProcessing', () => {
     expect(selectedBooking.mock.calls).toHaveLength(1);
     expect(selectedBooking.mock.calls[0][0]).toBe('6372221');
   });
+
+  it('should not call selectedBooking because url is not valid', () => {
+    const selectedBooking = jest.fn();
+
+    jsdom.reconfigure({
+      url: 'https://www.airplane.com/bookings/6372221?help=%2F',
+    });
+
+    mount(
+      <BookingHelper>
+        <SelectUrlBooking setSelected={selectedBooking} />
+      </BookingHelper>,
+    );
+
+    expect(selectedBooking.mock.calls).toHaveLength(0);
+  });
 });
