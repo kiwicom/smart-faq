2f9cebddcb56c8885d6acb648667e92527c3547e
Dino Trojak <dinotrojak@gmail.com>
2018-06-20 17:23:31 +0200

    renamed redirect component

--- src/IntroPage/index.js
+++ src/IntroPage/index.js
@@ -10,7 +10,7 @@ import CloseButton from './../common/buttons/CloseButton';
 import { withUser } from '../context/User';
 import type { User } from '../types';
 import FullFAQLink from '../common/FullFAQLink';
-import RedirectsLoggedIn from '../common/RedirectsLoggedIn';
+import RedirectsLoggedIn from '../common/RedirectLoggedIn';
 
 const style = css`
   .Intro {
