7b4af558236e4ca1fad1cb12db063d6372a0b0a1
Dino Trojak <dinotrojak@gmail.com>
2018-06-21 11:00:59 +0200

    refactored MMB url processing (#456)
    * fixed MMB url processing
    
    * moved back to old regex
    
    * updated tests
    
    * moved sleep to 45s
    
    * changed bookingId to number

--- src/common/integration/__tests__/urlProcessing.test.js
+++ src/common/integration/__tests__/urlProcessing.test.js
@@ -3,40 +3,53 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 
-import BookingStateProvider from '../../../context/BookingState';
+import { BookingState } from '../../../context/BookingState';
 import { SelectUrlBooking } from '../urlProcessing';
 
 describe('UrlProcessing', () => {
   it('should parse the url and return it as selected', () => {
     const selectedBooking = jest.fn();
+    const onSelectBooking = jest.fn();
 
     jsdom.reconfigure({
       url: 'https://www.kiwi.com/en/account/bookings/6372221?help=%2F',
     });
 
     mount(
-      <BookingStateProvider>
+      <BookingState.Provider
+        value={{
+          onSelectBooking,
+        }}
+      >
         <SelectUrlBooking setSelected={selectedBooking} />
-      </BookingStateProvider>,
+      </BookingState.Provider>,
     );
 
     expect(selectedBooking.mock.calls).toHaveLength(1);
-    expect(selectedBooking.mock.calls[0][0]).toBe('6372221');
+    expect(onSelectBooking.mock.calls).toHaveLength(1);
+    expect(selectedBooking.mock.calls[0][0]).toBe(6372221);
+    expect(onSelectBooking.mock.calls[0][0]).toBe(6372221);
   });
 
   it('should not call selectedBooking because url is not valid', () => {
     const selectedBooking = jest.fn();
+    const onSelectBooking = jest.fn();
 
     jsdom.reconfigure({
       url: 'https://www.airplane.com/bookings/6372221?help=%2F',
     });
 
     mount(
-      <BookingStateProvider>
+      <BookingState.Provider
+        value={{
+          onSelectBooking,
+        }}
+      >
         <SelectUrlBooking setSelected={selectedBooking} />
-      </BookingStateProvider>,
+      </BookingState.Provider>,
     );
 
     expect(selectedBooking.mock.calls).toHaveLength(0);
+    expect(onSelectBooking.mock.calls).toHaveLength(0);
   });
 });
