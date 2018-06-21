bfdbd925193886fd428e49a906a05e0cf5b58ef6
Dino Trojak <dinotrojak@gmail.com>
2018-06-21 16:36:46 +0200

    added our inMemory router to query param

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@
 import { Route, Switch, MemoryRouter } from 'react-router-dom';
 import * as React from 'react';
 
+import UrlSync from './UrlSync';
 import Intro from './IntroPage';
 import SignIn from './SignInPage';
 import KiwiLogin from './KiwiLogin';
@@ -17,19 +18,21 @@ type Props = {|
 const Routes = (props: Props) => {
   return (
     <MemoryRouter initialEntries={[props.initialRoute]} initialIndex={0}>
-      <Switch>
-        <Route exact path="/" component={Intro} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/kiwi-login" component={KiwiLogin} />
-        <Route path="/check-magic-link" component={CheckMagicLink} />
-        <Route path="/check-recovery-link" component={CheckRecoveryLink} />
-        <Route path="/forgotten-password" component={ForgottenPassword} />
-        <Route exact path="/faq/:categoryId?" component={ContentPage} />
-        <Route
-          path="/faq/:categoryId/article/:articleId"
-          component={ContentPage}
-        />
-      </Switch>
+      <UrlSync>
+        <Switch>
+          <Route exact path="/" component={Intro} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/kiwi-login" component={KiwiLogin} />
+          <Route path="/check-magic-link" component={CheckMagicLink} />
+          <Route path="/check-recovery-link" component={CheckRecoveryLink} />
+          <Route path="/forgotten-password" component={ForgottenPassword} />
+          <Route exact path="/faq/:categoryId?" component={ContentPage} />
+          <Route
+            path="/faq/:categoryId/article/:articleId"
+            component={ContentPage}
+          />
+        </Switch>
+      </UrlSync>
     </MemoryRouter>
   );
 };
