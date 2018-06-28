d1cca44454d33f6f578c3d329bd8c9c34360827a
Joan <jalbertsr@protonmail.ch>
2018-06-28 15:30:13 +0200

    fix(baggageinfo): fix height in firefox

--- src/StaticFAQ/FAQExtraInfo/BaggageInfo/index.js
+++ src/StaticFAQ/FAQExtraInfo/BaggageInfo/index.js
@@ -26,7 +26,6 @@ const styles = css`
   div.baggageCard {
     margin-top: 22px;
     width: 100%;
-    height: 100%;
     border-radius: 3px;
     background-color: #ffffff;
     border: solid 1px #e8edf1;
