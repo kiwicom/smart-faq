b735529dfa61856ed1de9a3eae728b4fdeaff36f
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-06-28 16:37:50 +0200

    feat(baggage-info): show baggage info (#495)
    * baggage info faq
    
    * baggage in fragments
    
    * feat(BaggageInfo): implement loader
    
    * feat(baggageInfo): refactor
    
    * feat(baggageinfo): context to toggle baggage
    
    * refactor(baggageinfo): test for baggage description
    
    * fix(baggageinfo): fix height in firefox

---
+++ src/StaticFAQ/FAQExtraInfo/BaggageInfo/BaggageDescription.js
@@ -0,0 +1,95 @@
+// @flow
+
+import * as React from 'react';
+import { graphql, createFragmentContainer } from 'react-relay';
+import { BaggageSmall, BaggageBig } from '@kiwicom/orbit-components/lib/icons';
+
+import type { BaggageDescription as BaggageDescriptionProps } from './__generated__/BaggageDescription.graphql';
+
+type Props = {|
+  type: 'Cabin' | 'Checked',
+  data: BaggageDescriptionProps,
+|};
+
+function renderIcon(type: string) {
+  switch (type) {
+    case 'Cabin':
+      return <BaggageSmall size="medium" customColor="#bac7d5" />;
+    case 'Checked':
+      return <BaggageBig size="medium" customColor="#bac7d5" />;
+  }
+  return null;
+}
+
+export const BaggageDescription = ({
+  type,
+  data: { height, weight, width, length },
+}: Props) => {
+  return (
+    <React.Fragment>
+      <div className="baggageRow">
+        {renderIcon(type)}
+        <p className="baggageWeight">
+          {type} baggage {weight} kg
+        </p>
+        <div className="baggageSize">
+          <p>
+            {height} x {width} x {length}
+          </p>
+        </div>
+      </div>
+      <hr className="separationLine" />
+      <style jsx>
+        {`
+          div.baggageRow {
+            padding: 15px 24px 15px 24px;
+          }
+          div.baggageSize {
+            display: inline-block;
+            float: right;
+          }
+          div.baggageSize p {
+            font-size: 12px;
+            line-height: 1.4;
+            color: #46515e;
+          }
+          div.baggageNumber {
+            display: inline-block;
+            margin-right: 8px;
+          }
+          div.baggageNumber p {
+            font-size: 14px;
+            font-weight: bold;
+            line-height: 1.4;
+            color: #46515e;
+            display: inline-block;
+          }
+          p.baggageWeight {
+            margin-left: 5px;
+            font-size: 14px;
+            font-weight: bold;
+            line-height: 1.4;
+            color: #46515e;
+            display: inline-block;
+          }
+          hr.separationLine {
+            border: solid 1px #e8edf1;
+            width: 100%;
+          }
+        `}
+      </style>
+    </React.Fragment>
+  );
+};
+
+export default createFragmentContainer(
+  BaggageDescription,
+  graphql`
+    fragment BaggageDescription on Baggage {
+      height
+      weight
+      width
+      length
+    }
+  `,
+);
---
+++ src/StaticFAQ/FAQExtraInfo/BaggageInfo/BaggageSummary.js
@@ -0,0 +1,46 @@
+// @flow
+
+import idx from 'idx';
+import * as React from 'react';
+import { graphql, createFragmentContainer } from 'react-relay';
+
+import BaggageLoader from './BaggageLoader';
+import BaggageDescriptionFragment from './BaggageDescription';
+import type { BaggageSummary as BaggageSummaryProps } from './__generated__/BaggageSummary.graphql';
+
+type Props = {|
+  data: BaggageSummaryProps,
+|};
+
+const BaggageSummary = ({ data }: Props) => {
+  const cabinBaggage = idx(data, _ => _.cabin) || [];
+  const checkedBaggage = idx(data, _ => _.checked) || [];
+  return data ? (
+    <React.Fragment>
+      {checkedBaggage.map((bagagge, i) => (
+        /* eslint-disable react/no-array-index-key*/
+        <BaggageDescriptionFragment key={i} data={bagagge} type="Checked" />
+      ))}
+      {cabinBaggage.map((bagagge, i) => (
+        /* eslint-disable react/no-array-index-key*/
+        <BaggageDescriptionFragment key={i} data={bagagge} type="Cabin" />
+      ))}
+    </React.Fragment>
+  ) : (
+    <BaggageLoader />
+  );
+};
+
+export default createFragmentContainer(
+  BaggageSummary,
+  graphql`
+    fragment BaggageSummary on AllowedBaggage {
+      checked {
+        ...BaggageDescription
+      }
+      cabin {
+        ...BaggageDescription
+      }
+    }
+  `,
+);
