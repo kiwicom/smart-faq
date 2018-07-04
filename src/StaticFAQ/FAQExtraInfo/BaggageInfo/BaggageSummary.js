59e2fe7f25c380ce9e261906ce40a933f505a43f
Daniel Kantor <git@daniel-kantor.com>
2018-07-04 18:46:06 +0200

    revert: revert "revert: revert "WIP: Fix issues on 453 (#516)" (#518)"
    This reverts commit 0084176c0951b0d8f5dac0b1e3e64046c208ce76.

--- src/StaticFAQ/FAQExtraInfo/BaggageInfo/BaggageSummary.js
+++ src/StaticFAQ/FAQExtraInfo/BaggageInfo/BaggageSummary.js
@@ -3,14 +3,29 @@
 import idx from 'idx';
 import * as React from 'react';
 import { graphql, createFragmentContainer } from 'react-relay';
+import css from 'styled-jsx/css';
 
 import BaggageLoader from './BaggageLoader';
 import BaggageDescriptionFragment from './BaggageDescription';
+// import replaceWithCurrentDomain from '../../../helpers/replaceWithCurrentDomain';
 import type { BaggageSummary as BaggageSummaryProps } from './__generated__/BaggageSummary.graphql';
 
 type Props = {|
   data: BaggageSummaryProps,
 |};
+
+const styles = css`
+  .moreInfo {
+    padding-left: 24px;
+    padding-bottom: 24px;
+  }
+  .moreInfo a {
+    color: #00a991;
+    font-size: 12px;
+    text-decoration: none;
+    font-weight: 500;
+  }
+`;
 
 const BaggageSummary = ({ data }: Props) => {
   const cabinBaggage = idx(data, _ => _.cabin) || [];
@@ -25,6 +40,17 @@ const BaggageSummary = ({ data }: Props) => {
         /* eslint-disable react/no-array-index-key*/
         <BaggageDescriptionFragment key={i} data={bagagge} type="Cabin" />
       ))}
+      <div className="moreInfo">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          // href={replaceWithCurrentDomain(booking.directAccessURL)}
+          href="/"
+        >
+          More Info
+        </a>
+      </div>
+      <style jsx>{styles}</style>
     </React.Fragment>
   ) : (
     <BaggageLoader />
