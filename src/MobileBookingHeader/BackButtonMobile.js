950a53e818b68a3a5cda6362e32e9a812a0e1011
Daniel Kantor <git@daniel-kantor.com>
2018-07-13 15:36:47 +0200

    fix(mobile-back-button): don't show back button when it's not active

--- src/MobileBookingHeader/BackButtonMobile.js
+++ src/MobileBookingHeader/BackButtonMobile.js
@@ -2,27 +2,8 @@
 
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
-import idx from 'idx';
 import css from 'styled-jsx/css';
 import { ChevronLeft } from '@kiwicom/orbit-components/lib/icons';
-
-type Props = {|
-  history: {
-    goBack: () => void,
-    push: string => void,
-    location: {
-      pathname: string,
-    },
-    entries: Array<{
-      pathname: string,
-    }>,
-  },
-  match: {
-    params: {
-      categoryId: ?string,
-    },
-  },
-|};
 
 const BackButtonMobileStyle = css`
   .back {
@@ -38,20 +19,11 @@ const BackButtonMobileStyle = css`
   }
 `;
 
-const BackButtonMobile = (props: Props) => {
-  const { location } = props.history;
-  const hasCategory = idx(props.match, _ => _.params.categoryId) || null;
-  const currentpath = location && location.pathname;
-  const isArticle = currentpath.includes('article/');
-
-  return (
-    <div data-cy="back-button" className="back">
-      {hasCategory || isArticle ? (
-        <ChevronLeft size="medium" customColor="#45505d" />
-      ) : null}
-      <style jsx>{BackButtonMobileStyle}</style>
-    </div>
-  );
-};
+const BackButtonMobile = () => (
+  <div data-cy="back-button" className="back">
+    <ChevronLeft size="medium" customColor="#45505d" />
+    <style jsx>{BackButtonMobileStyle}</style>
+  </div>
+);
 
 export default withRouter(BackButtonMobile);
