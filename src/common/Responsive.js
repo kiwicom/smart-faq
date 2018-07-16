899187c684016ce33898d8ce4fb508f1ad6f1816
Joan <jalbertsr@protonmail.ch>
2018-07-16 14:56:44 +0200

    fix(responsive): fix typo
    fixes #502
    fixes #538 
    fixes #512

--- src/common/Responsive.js
+++ src/common/Responsive.js
@@ -10,13 +10,13 @@ type Props = {|
 const breakPoint = 900;
 
 export const Desktop = ({ children }: Props) => (
-  <MediaQuery query={`screen and (min-width: ${breakPoint + 1}px`}>
+  <MediaQuery query={`screen and (min-width: ${breakPoint + 1}px)`}>
     {children}
   </MediaQuery>
 );
 
 export const Mobile = ({ children }: Props) => (
-  <MediaQuery query={`screen and (max-width: ${breakPoint}px`}>
+  <MediaQuery query={`screen and (max-width: ${breakPoint}px)`}>
     {children}
   </MediaQuery>
 );
