c9476ae12d4f6f599030c2b8b176c11e2a511462
Daniel Kantor <git@daniel-kantor.com>
2018-07-20 09:14:57 +0200

    test(mobile-booking-header): fix cypress test

--- cypress/integration/MobileBookingLayout.js
+++ cypress/integration/MobileBookingLayout.js
@@ -9,23 +9,23 @@ describe('Mobile Booking Layout', () => {
     cy.get('[data-cy=btn-existent-booking]').click();
     cy.get('[data-cy=link-kiwi-login]').click();
 
-    cy.get('[data-cy=input-email]').type('test@kiwi.com');
-    cy.get('[data-cy=input-password]').type('kiwitest');
+    cy.get('[data-cy=input-email]').type(Cypress.env('TEST_USER_EMAIL'));
+    cy.get('[data-cy=input-password]').type(Cypress.env('TEST_USER_PASSWORD'));
     cy.get('[data-cy=btn-sign-in] button').click();
-
-    cy.contains('Manage My Booking').should('not.be.visible');
-    cy.contains('Select another booking').should('not.be.visible');
-    cy.get('.TripDescription').should('not.be.visible');
 
     cy.get('[data-cy=trip-button]').click();
     cy.get('.TripDescription').should('be.visible');
-
     cy.contains(/(Upcoming|Past) trip #[0-9 ]+/).should('be.visible');
-    cy.contains(/(Upcoming|Past) trip #[0-9 ]+/).click();
 
     cy.contains('Manage My Booking').should('be.visible');
     cy.contains('Select another booking').should('be.visible');
 
+    cy.contains(/(Upcoming|Past) trip #[0-9 ]+/).click();
+
+    cy.contains('Manage My Booking').should('not.be.visible');
+    cy.contains('Select another booking').should('not.be.visible');
+
+    cy.contains(/(Upcoming|Past) trip #[0-9 ]+/).click();
     cy.contains('Select another booking').click();
 
     cy
@@ -38,5 +38,7 @@ describe('Mobile Booking Layout', () => {
       .find('.bookingCard')
       .eq(3)
       .click();
+
+    cy.get('.TripDescription').should('not.be.visible');
   });
 });
