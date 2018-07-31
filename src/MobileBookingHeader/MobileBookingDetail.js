75eb7015c02befa61ffc8351c04394334fa34cde
Daniel Kantor <git@daniel-kantor.com>
2018-07-31 18:17:43 +0200

    feat(mobile-booking-header): implement landscape for booking detail

--- src/MobileBookingHeader/MobileBookingDetail.js
+++ src/MobileBookingHeader/MobileBookingDetail.js
@@ -15,6 +15,7 @@ import ReturnTrip from './ReturnTrip';
 import MultiCityTrip from './MultiCityTrip';
 import formatBookingId from '../helpers/formatBookingId';
 import replaceWithCurrentDomain from '../helpers/replaceWithCurrentDomain';
+import { Portrait, Landscape } from '../common/Responsive';
 
 type ContextProps = {
   onSetFAQSection: (isUrgent: boolean, isPastBooking: boolean) => void,
@@ -51,50 +52,82 @@ const MobileBookingControlsStyle = css`
   }
 `;
 
+type TripIdProps = {|
+  isPastBooking: boolean,
+  databaseId: number,
+|};
+
+const TripId = (props: TripIdProps) =>
+  props.databaseId && (
+    <div className="TripId">
+      <span>
+        {`${props.isPastBooking ? 'Past' : 'Upcoming'} trip # ${formatBookingId(
+          props.databaseId,
+        )}`}
+      </span>
+      <style jsx>
+        {`
+          .TripId {
+            font-size: 14px;
+            line-height: 1.4;
+            color: #7f91a8;
+            margin-top: 8px;
+          }
+        `}
+      </style>
+    </div>
+  );
+
+type ManageMyBookingButtonProps = {|
+  +manageBookingURL: ?string,
+|};
+
+const ManageMyBookingButton = (props: ManageMyBookingButtonProps) => (
+  <div className="manageBookingButton">
+    <Button
+      block
+      onClick={() => {
+        window.open(props.manageBookingURL, '_blank', 'noopener');
+      }}
+      type="secondary"
+      size="small"
+    >
+      Manage My Booking
+    </Button>
+    <style jsx>{MobileBookingControlsStyle}</style>
+  </div>
+);
+
+const SelectAnotherBookingButton = () => (
+  <div className="selectBookingButton">
+    <BookingState.Consumer>
+      {({ onDisplayAll }: BookingStateType) => (
+        <div data-cy="btn-other-bookings">
+          <TextLink
+            onClick={e => {
+              e.preventDefault();
+              onDisplayAll();
+            }}
+            external={false}
+            type="primary"
+          >
+            Select another booking
+          </TextLink>
+        </div>
+      )}
+    </BookingState.Consumer>
+    <style jsx>{MobileBookingControlsStyle}</style>
+  </div>
+);
+
 const MobileBookingControls = (props: MobileBookingControlsProps) => (
   <React.Fragment>
-    <div className="manageBookingButton">
-      <Button
-        block
-        onClick={() => {
-          window.open(props.manageBookingURL, '_blank', 'noopener');
-        }}
-        type="secondary"
-        size="small"
-      >
-        Manage My Booking
-      </Button>
-    </div>
-    <div className="selectBookingButton">
-      <BookingState.Consumer>
-        {({ onDisplayAll }: BookingStateType) => (
-          <div data-cy="btn-other-bookings">
-            <TextLink
-              onClick={e => {
-                e.preventDefault();
-                onDisplayAll();
-              }}
-              external={false}
-              type="primary"
-            >
-              Select another booking
-            </TextLink>
-          </div>
-        )}
-      </BookingState.Consumer>
-    </div>
-    <style jsx>{MobileBookingControlsStyle}</style>
+    <ManageMyBookingButton manageBookingURL={props.manageBookingURL} />
+    <SelectAnotherBookingButton />
   </React.Fragment>
 );
 
 const MobileBookingSummaryStyle = css`
-  .TripId {
-    font-size: 14px;
-    line-height: 1.4;
-    color: #7f91a8;
-    margin-top: 8px;
-  }
-
   .topRow {
     display: flex;
   }
@@ -115,7 +148,71 @@ type State = {
   expanded: boolean,
 };
 
-class MobileBookingDetail extends React.Component<Props, State> {
+const renderByType = booking => {
+  if (booking.type === bookingTypes.ONE_WAY) {
+    return <OneWayTrip booking={booking} />;
+  }
+
+  if (booking.type === bookingTypes.RETURN) {
+    return <ReturnTrip booking={booking} />;
+  }
+
+  if (booking.type === bookingTypes.MULTICITY) {
+    return <MultiCityTrip booking={booking} />;
+  }
+
+  return null;
+};
+
+class LandscapeMobileBookingDetail extends React.Component<Props, State> {
+  componentDidMount() {
+    updateFAQSection(this.props);
+  }
+
+  componentDidUpdate() {
+    updateFAQSection(this.props);
+  }
+
+  render() {
+    const { booking } = this.props;
+    const { isPastBooking, databaseId } = booking;
+    return (
+      <div>
+        <div className="flexToolbar">
+          <div>
+            <TripId databaseId={databaseId} isPastBooking={isPastBooking} />
+            {renderByType(booking)}
+          </div>
+          <div>
+            <SelectAnotherBookingButton />
+          </div>
+        </div>
+        <div className="centered">
+          <ManageMyBookingButton
+            manageBookingURL={
+              booking.directAccessURL &&
+              replaceWithCurrentDomain(booking.directAccessURL)
+            }
+          />
+        </div>
+        <style jsx>
+          {`
+            .centered {
+              max-width: 288px;
+              margin: auto;
+            }
+            .flexToolbar {
+              display: flex;
+              justify-content: space-between;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+}
+
+class PortraitMobileBookingDetail extends React.Component<Props, State> {
   state = {
     expanded: true,
   };
@@ -127,22 +224,6 @@ class MobileBookingDetail extends React.Component<Props, State> {
   componentDidUpdate() {
     updateFAQSection(this.props);
   }
-
-  renderByType = booking => {
-    if (booking.type === bookingTypes.ONE_WAY) {
-      return <OneWayTrip booking={booking} />;
-    }
-
-    if (booking.type === bookingTypes.RETURN) {
-      return <ReturnTrip booking={booking} />;
-    }
-
-    if (booking.type === bookingTypes.MULTICITY) {
-      return <MultiCityTrip booking={booking} />;
-    }
-
-    return null;
-  };
 
   toggle() {
     this.setState(prevState => ({ expanded: !prevState.expanded }));
@@ -161,14 +242,8 @@ class MobileBookingDetail extends React.Component<Props, State> {
           tabIndex="0"
         >
           <div style={{ flexGrow: 1 }}>
-            {databaseId && (
-              <div className="TripId">
-                {`${
-                  isPastBooking ? 'Past' : 'Upcoming'
-                } trip # ${formatBookingId(databaseId)}`}
-              </div>
-            )}
-            {this.renderByType(booking)}
+            <TripId databaseId={databaseId} isPastBooking={isPastBooking} />
+            {renderByType(booking)}
           </div>
           <div className="Chevron">
             {this.state.expanded ? (
@@ -197,7 +272,20 @@ class MobileBookingDetail extends React.Component<Props, State> {
 const MobileBookingDetailWithFAQHandler = (props: ComponentProps) => (
   <BookingState.Consumer>
     {({ onSetFAQSection }: ContextProps) => (
-      <MobileBookingDetail {...props} onSetFAQSection={onSetFAQSection} />
+      <React.Fragment>
+        <Portrait>
+          <PortraitMobileBookingDetail
+            {...props}
+            onSetFAQSection={onSetFAQSection}
+          />
+        </Portrait>
+        <Landscape>
+          <LandscapeMobileBookingDetail
+            {...props}
+            onSetFAQSection={onSetFAQSection}
+          />
+        </Landscape>
+      </React.Fragment>
     )}
   </BookingState.Consumer>
 );
