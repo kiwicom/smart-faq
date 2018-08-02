bd40eaad07a7204a011534b0c847b95868955987
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2018-08-02 11:51:58 +0200

    fix: make server side rendering work (#609)

--- src/helpers/replaceWithCurrentDomain.js
+++ src/helpers/replaceWithCurrentDomain.js
@@ -3,7 +3,12 @@
 export const replaceDomain = (link: string, domain: string) =>
   link.replace(/(https?:\/\/)[^/]+(.*)/, `${domain}$2`);
 
-const replaceWithCurrentDomain = (url: string) =>
-  replaceDomain(url, window.location.origin);
+const replaceWithCurrentDomain = (url: string) => {
+  if (typeof window === 'undefined') {
+    return url;
+  }
+
+  return replaceDomain(url, window.location.origin);
+};
 
 export default replaceWithCurrentDomain;
