43db3eaadcff0618033534d356e6e1f53c8dd1b7
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2018-08-08 10:07:07 +0200

    refactor: remove all cy.wait() calls

--- cypress/integration/QueryParamsRedirects.js
+++ cypress/integration/QueryParamsRedirects.js
@@ -4,7 +4,6 @@
 describe('Query params redirect to correct article', () => {
   it(`User should navigate to a FAQ article with url`, () => {
     cy.visit('/?help=/faq/RkFRQ2F0ZWdvcnk6ODk=/article/RkFRQXJ0aWNsZTo0MQ==');
-    cy.wait(5000);
   });
 
   it(`Breadcrumbs should have length of 4`, () => {
