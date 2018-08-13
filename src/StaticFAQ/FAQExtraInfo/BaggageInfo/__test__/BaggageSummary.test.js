35430314619bcb771c7148759df725f928a4006e
Daniel Kantor <git@daniel-kantor.com>
2018-08-13 16:47:17 +0200

    test(baggage-summary): test rendering all baggage

--- src/StaticFAQ/FAQExtraInfo/BaggageInfo/__test__/BaggageSummary.test.js
+++ src/StaticFAQ/FAQExtraInfo/BaggageInfo/__test__/BaggageSummary.test.js
@@ -1,14 +1,33 @@
 // @flow
 
 import * as React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 
 import { RawBaggageSummary } from '../BaggageSummary';
+
+const mockRefType: any = null;
 
 describe('BaggageSummary', () => {
   it('loading screen renders correctly', () => {
     expect(
       render(<RawBaggageSummary data={null} mmbUrl="https://example.com" />),
+    ).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const data = [
+      {
+        $refType: mockRefType,
+        $fragmentRefs: mockRefType,
+      },
+      {
+        $refType: mockRefType,
+        $fragmentRefs: mockRefType,
+      },
+    ];
+
+    expect(
+      shallow(<RawBaggageSummary data={data} mmbUrl="https://example.com" />),
     ).toMatchSnapshot();
   });
 });
