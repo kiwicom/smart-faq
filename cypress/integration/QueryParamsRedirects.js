87e94250f4c0e180556f472906e6c56bc2e26603
Viktor Borszeki <vborszeki@gmail.com>
2018-08-13 16:38:46 +0200

    test(integration): extend query params redirect test

--- cypress/integration/QueryParamsRedirects.js
+++ cypress/integration/QueryParamsRedirects.js
@@ -2,11 +2,11 @@
 /* global cy */
 
 describe('Query params redirect to correct article', () => {
-  it(`User should navigate to a FAQ article with url`, () => {
+  it(`should navigate to an FAQ article with given url`, () => {
     cy.visit('/?help=/faq/RkFRQ2F0ZWdvcnk6ODk=/article/RkFRQXJ0aWNsZTo0MQ==');
   });
 
-  it(`Breadcrumbs should have length of 4`, () => {
+  it(`should have correct amount of breadcrumbs`, () => {
     cy.get('[data-cy=faq-breadcrumbs]').as('faq-breadcrumbs');
     cy
       .get('@faq-breadcrumbs')
@@ -14,16 +14,30 @@ describe('Query params redirect to correct article', () => {
       .should('have.length', 3);
   });
 
-  it(`Feedback form should be present`, () => {
+  it(`should have feedback form`, () => {
     cy.get('[data-cy=faq-article-form]').as('faq-article-form');
 
     cy.get('@faq-article-form').should('exist');
   });
 
-  it(`Article title should be corect`, () => {
+  it(`should display correct article title`, () => {
     cy
       .get('.faq-article-detail')
       .find('h1')
       .contains('Cabin baggage');
+  });
+
+  it('should display the root FAQ categories after clicking home breadcrumb', () => {
+    cy
+      .get('[data-cy=faq-breadcrumbs]')
+      .contains('Home')
+      .click();
+
+    cy
+      .get('[data-cy=faq-categories]')
+      .children()
+      .first()
+      .contains('Baggage')
+      .should('exist');
   });
 });
