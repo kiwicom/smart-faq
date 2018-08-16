97648b127298ba235e5badf6307b5b7fd64f65bf
Viktor Borszeki <vborszeki@gmail.com>
2018-08-16 17:58:03 +0200

    refactor(emergency): use orbit alert component

--- src/StaticFAQ/emergencies/Emergency.js
+++ src/StaticFAQ/emergencies/Emergency.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { Text } from '@kiwicom/orbit-components';
-import Alert from '@kiwicom/orbit-components/lib/icons/Alert';
+import Alert from '@kiwicom/orbit-components/lib/Alert';
 import css from 'styled-jsx/css';
 
 import Card from '../../common/Card';
@@ -13,30 +13,18 @@ type Props = {|
 |};
 
 const style = css`
-  .icon {
-    position: absolute;
-    top: calc(50% - 12px);
-  }
-  .emergencyText {
-    overflow: hidden;
-    max-height: 100%;
-    line-height: 1.4;
-    margin-left: 38px;
+  .warning {
+    margin-top: 24px;
   }
 `;
 
 const Emergency = ({ emergency }: Props) => (
-  <Card>
-    <div className="icon">
-      <Alert />
-    </div>
-    <div className="emergencyText" data-cy="emergency">
-      <Text type="secondary" size="small" element="span">
-        {emergency}
-      </Text>
-    </div>
+  <div className="warning">
+    <Alert type="warning" title={null} icon>
+      {emergency}
+    </Alert>
     <style jsx>{style}</style>
-  </Card>
+  </div>
 );
 
 export default Emergency;
