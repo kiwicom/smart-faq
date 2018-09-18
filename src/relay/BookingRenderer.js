4f08a780b7b3b27d04d8a0cea430253baae8ec6a
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2018-09-18 11:33:48 +0200

    fix(chat): fix guarantee resolver for selected booking page

--- src/relay/BookingRenderer.js
+++ src/relay/BookingRenderer.js
@@ -6,6 +6,7 @@ import * as React from 'react';
 import QueryRenderer from './QueryRenderer';
 import GuaranteeNeededResolver from './GuaranteeNeededResolver';
 import type { Props as QueryRendererProps } from './QueryRenderer';
+import bookingTypes from '../common/booking/bookingTypes';
 
 type RenderState<RenderProps> = {
   props: ?RenderProps,
@@ -15,14 +16,31 @@ type RenderState<RenderProps> = {
 class BookingRenderer<RenderProps> extends React.Component<
   QueryRendererProps<RenderProps>,
 > {
+  getBookingByType = (booking: Object) => {
+    switch (booking.type) {
+      case bookingTypes.ONE_WAY:
+        return booking.oneWay;
+      case bookingTypes.RETURN:
+        return booking.return;
+      case bookingTypes.MULTICITY:
+        return booking.multicity;
+    }
+  };
+
   renderBooking = (renderState: RenderState<RenderProps>) => {
-    const guaranteeChatBookingInfo =
-      // $FlowExpectedError: Expected
-      idx(renderState.props, _ => _.booking) ||
+    let guaranteeChatBookingInfo =
       // $FlowExpectedError: Expected
       idx(renderState.props, _ => _.singleBooking) ||
       // $FlowExpectedError: Expected
       idx(renderState.props, _ => _.nearestBooking);
+    if (!guaranteeChatBookingInfo) {
+      // $FlowExpectedError: Expected
+      const booking = idx(renderState.props, _ => _.booking);
+
+      if (booking) {
+        guaranteeChatBookingInfo = this.getBookingByType(booking);
+      }
+    }
 
     return (
       <React.Fragment>
